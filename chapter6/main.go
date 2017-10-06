16577cc6f9088fb5da4ab08affbcf1116ea7ed6d
Jennifer Brabec <jennifer.brabec@echtra.net>
2017-10-05 20:26:07 -0700

    ch. 6 complete through ex. 5

---
+++ chapter6/main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func average(xs []float64) float64 {
+//  total := 0.0
+//  for _, v := range xs {
+//      total += v
+//  }
+//  return total / float64(len(xs))
+// }
+
+// func main() {
+//  xs := []float64{98, 93, 77, 82, 83}
+//  fmt.Println(average(xs))
+// }
+
+// func f() (int, int) {
+//  return 5, 6
+// }
+
+// func main() {
+//  x, y := f()
+//  fmt.Println(x, y)
+// }
+
+// func add(args ...int) int {
+//  total := 0
+//  for _, v := range args {
+//      total += v
+//  }
+//  return total
+// }
+
+// func main() {
+//  // fmt.Println(add(1, 2, 3))
+//  xs := []int{1, 2, 3}
+//  fmt.Println(add(xs...))
+// }
+
+// func makeEvenGenerator() func() uint {
+//  i := uint(0)
+//  return func() (ret uint) {
+//      ret = i
+//      i += 2
+//      return
+//  }
+// }
+// func main() {
+//  nextEven := makeEvenGenerator()
+//  fmt.Println(nextEven()) // 0
+//  fmt.Println(nextEven()) // 2
+//  fmt.Println(nextEven()) // 4
+// }
+
+// func factorial(x uint) uint {
+//  if x == 0 {
+//      return 1
+//  }
+//  return x * factorial(x-1)
+// }
+// func main() {
+//  fmt.Println(factorial(5))
+// }
+
+// func main() {
+//  defer func() {
+//      str := recover()
+//      fmt.Println(str)
+//  }()
+//  panic("PANIC")
+// }
+
+// func zero(x int) {
+//  x = 0
+// }
+
+// func main() {
+//  x := 5
+//  fmt.Println(x) // x is 5
+//  zero(x)
+//  fmt.Println(x) // x is still 5
+// }
+
+// func zero(xPtr *int) {
+//  *xPtr = 0
+// }
+// func main() {
+//  x := 5
+//  fmt.Println(x) // x is 5
+//  zero(&x)
+//  fmt.Println(x) // x is 0
+// }
+
+// func one(xPtr *int) {
+//  *xPtr = 1
+// }
+// func main() {
+//  xPtr := new(int)
+//  fmt.Println(*xPtr) // x is 0
+//  one(xPtr)
+//  fmt.Println(*xPtr) // x is 1
+// }
+
+// Exercises:
+// func sum(xs []int) int
+
+// func half(x int) (int, bool) {
+//  return x / 2, x%2 == 0
+// }
+// func main() {
+//     fmt.Println(half(2))
+// }
+
+// func findMax(args ...int) int {
+//  var max int
+//  for i, x := range args {
+//      if i == 0 || x > max {
+//          max = x
+//      }
+//  }
+//  return max
+// }
+// func main() {
+//  fmt.Println(findMax(1, 2, 3, 4, 5, 2))
+// }
+
+// func makeOddGenerator() func() uint {
+//  i := uint(1)
+//  return func() (ret uint) {
+//      ret = i
+//      i += 2
+//      return
+//  }
+// }
+// func main() {
+//  nextOdd := makeOddGenerator()
+//  fmt.Println(nextOdd()) // 1
+//  fmt.Println(nextOdd()) // 3
+//  fmt.Println(nextOdd()) // 5
+// }
+
+func fib(n int) int {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		return fib(n-1) + fib(n-2)
+	}
+}
+func main() {
+	fmt.Println(fib(1))
+}
